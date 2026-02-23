2f37566d57aadb9f3f748613e401859a9f7734a6
dan-acsc <130635354+dan-acsc@users.noreply.github.com>
2026-02-23 13:41:27 +1100

    feat: add an additional admin role utility. (#7)
    feat: add an additional admin role utility.
    
    
    [AB#4411](https://dev.azure.com/MSFT-ACSC/d647a5d6-6131-437d-81dc-fb931aec36b8/_workitems/edit/4411)
    
    ---------
    
    Co-authored-by: dan-acsc <dan-acsc@users.noreply.github.com>

--- azul_security/admin.py
+++ azul_security/admin.py
@@ -1,22 +1,26 @@
 """Provide functionality to verify if a user is an admin or not."""
 
 import functools
-from typing import Any
+
+from azul_bedrock.models_auth import UserInfo
 
 from azul_security.settings import Settings
 
 
 @functools.lru_cache()
-def _get_settings():
+def get_settings():
     """Return an instance of the azul-security settings class, cached."""
     return Settings()
 
 
-def is_user_admin(user_info: Any) -> bool:
-    """Return true if the user is admin and false if they are not."""
-    # user_info is expected to be the models_auth.UserInfo type however we wish to avoid adding another dependency.
-    # Unit tests will pick up problems if the underlying model changes.
-    for admin_role in _get_settings().admin_roles:
-        if admin_role in user_info.roles:
+def is_admin_roles(roles: list[str]) -> bool:
+    """Verify if the list of provided roles contains an admin role."""
+    for admin_role in get_settings().admin_roles:
+        if admin_role in roles:
             return True
     return False
+
+
+def is_user_admin(user_info: UserInfo) -> bool:
+    """Return true if the user is admin and false if they are not."""
+    return is_admin_roles(user_info.roles)
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -3,7 +3,7 @@ import unittest
 from azul_bedrock.models_auth import UserInfo
 from fastapi import Request
 
-from azul_security.admin import is_user_admin
+from azul_security.admin import is_user_admin, is_admin_roles
 
 from . import support
 
@@ -30,9 +30,12 @@ class TestAdmin(unittest.TestCase):
 
     def test_admin_user(self):
         self.assertTrue(is_user_admin(self._create_user_info(["admin"])))
+        self.assertTrue(is_admin_roles(["admin"]))
 
     def test_not_admin_user(self):
         self.assertFalse(is_user_admin(self._create_user_info(["validated"])))
+        self.assertFalse(is_admin_roles(["validated"]))
 
     def test_not_label_admin_user(self):
         self.assertFalse(is_user_admin(self._create_user_info([])))
+        self.assertFalse(is_admin_roles([]))
